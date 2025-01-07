51a48a45a23a295b65b581053389d960dc188f93
luigigh <luigifalconibruno@gmail.com>
2025-01-07 13:49:22 -0300

    Added Image and Text in HomePage

--- app/index.jsx
+++ app/index.jsx
@@ -11,6 +11,21 @@ export default function App() {
       <ScrollView contentContainerStyle={{height: '100%'}}>
         <View className='w-full items-center h-full p-4'>
           <Image source={images.logo} className='w-[130px] h-[84px]' resizeMode="contain"/>
+          <Image source={images.cards} className="max-w-[380px] w-full h-[300px]" resizeMode="contain"/>
+
+          <View className='relative mt-5'>
+            <Text className='text-3xl text-white font-bold text-center'>
+              Discover Endless Possibilities with {' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+
+            <Image source={images.path} className='w-[136px] h-[15px] absolute -bottom-2 -right-8' resizeMode='contain'/>
+          </View>
+
+          <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Where criativity meets Inovation: Imbark on a journey of exploration with aora</Text>
+
+          
+
         </View>
       </ScrollView>
     </SafeAreaView>
