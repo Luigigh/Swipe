3ab2c3d0d827b19947203296fe7c7883dfa5e599
luigigh <luigifalconibruno@gmail.com>
2025-01-07 20:03:52 -0300

    Start developing sing-in screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Image } from 'react-native'
+import React, {useState} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+import {images} from '../../constants';
+
 
 const SignIn = () => {
+
+  const [form, setform] = useState({email: '', password: ''})
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+
+      <SafeAreaView className='bg-primary h-full'>
+        <ScrollView>
+
+          <View className=' bg-white w-full justify-center h-full p-4 my-6'>
+
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[130px] h-[84px]"
+          />
+
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold">
+            Log in to Aora
+          </Text>
+
+          
+          </View>
+        </ScrollView>
+
+
+      </SafeAreaView>
   )
 }
 
